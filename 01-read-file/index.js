0bdfaba438b335e229281c48d835f774b42f8090
KozerenkoRoman <kozerenko@gmail.com>
2021-11-04 01:34:47 +0200

    feat add 01 task

---
+++ 01-read-file/index.js
@@ -0,0 +1,28 @@
+const chalk = require('chalk'); //https://www.npmjs.com/package/chalk
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+
+class FileReader {
+    stdout = process.stdout;
+
+    constructor(fileName) {
+        this.file = path.join(__dirname, fileName);
+    }
+
+    readFile() {
+        const readableStream = fs.createReadStream(this.file, 'utf-8');
+        readableStream.on('data', (chunk) => {
+            this.stdout.write(chalk.bold(chunk))
+        });
+    }
+
+    sayGoodBye() {
+        process.on('exit', () => this.stdout.write(chalk.blueBright('Good luck!\n')));
+    }
+}
+
+const fileReader = new FileReader('text.txt');
+fileReader.readFile();
+fileReader.sayGoodBye();
+
