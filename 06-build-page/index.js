162dd12e34fa7c1a77cb0655c08135183101df97
KozerenkoRoman <kozerenko@gmail.com>
2021-11-07 16:57:43 +0200

    feat: createHtmlFile

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,5 +1,6 @@
 const chalk = require('chalk'); //https://www.npmjs.com/package/chalk
-const fs = require('fs').promises;
+const fs = require('fs');
+const fsp = require('fs').promises;
 const path = require('path');
 const process = require('process');
 
@@ -12,63 +13,96 @@ class PageBuilder {
     }
 
     async build() {
-        console.log('enter Build');
-        this.makeDir(this.projectDir).then(() => {
-            console.log('enter make dir ' + this.projectDir);
+        await this.makeDir(this.projectDir).then(() => {
             const assets = path.join(this.projectDir, '/assets');
             this.makeDir(assets).then(() => {
-                this.makeDir(path.join(assets, '/fonts'));
-                this.makeDir(path.join(assets, '/img'));
-                this.makeDir(path.join(assets, '/svg'));
+                const fonts = path.join(assets, '/fonts');
+                const img = path.join(assets, '/img');
+                const svg = path.join(assets, '/svg')
+                this.makeDir(fonts).then(() => {
+                    this.copyDir(path.join(__dirname, '/assets/fonts'), fonts);
+                });
+                this.makeDir(img).then(() => {
+                    this.copyDir(path.join(__dirname, '/assets/img'), img);
+                });
+                this.makeDir(svg).then(() => {
+                    this.copyDir(path.join(__dirname, '/assets/svg'), svg);
+                });
             });
         }).then(() => {
-        })
+            this.createHtmlFile();
+        }).then(() => {
+            this.createCssFile();
+        });
     }
 
     async makeDir(dir) {
-        console.log(`Enter to make ${dir}`);
-        await fs.rm(dir, {recursive: true}, (err) => {
-            if (!err) console.log(chalk.blue(`Directory ${dir} deleted`));
-        });
-        await fs.mkdir(dir, {recursive: true}, (err) => {
-            if (err) console.log(chalk.red(err));
-            console.log(chalk.green(`Directory ${dir} created`));
-        });
-    }
-
-    copyDir(source, destDir) {
-        fs.readdir(source, {withFileTypes: true}, (err, files) => {
-            if (err) console.log(chalk.red(err));
-            files.forEach((file) => {
-                if (file.isFile()) {
-                    this.copyFile(file, destDir);
-                }
-            });
-        });
-    }
-
-    async copyFile(source, destDir) {
-        await fs.copyFile(source, destDir, (err) => {
-            if (err) throw err;
-            console.log(`File ${source} copied to ${destDir}`);
-        })
-    }
-
-    async readFile(fileName) {
         try {
-            const data = await fs.readFile(fileName);
-            console.log(data.toString());
-            return data.toString();
+            await fsp.rm(dir, {recursive: true}, (err) => {
+                console.log(chalk.blue(`Directory ${dir} deleted`));
+            })
         } catch (err) {
-            console.error(`Got an error trying to read the file ${fileName}: ${err.message}`);
+            console.log(chalk.blue(`No such directory ${dir} for delete`));
+        }
+        try {
+            await fsp.mkdir(dir, {recursive: true}, (err) => {
+                console.log(chalk.green(`Directory ${dir} created`));
+            })
+        } catch (err) {
+            console.log(chalk.red(`Unable to create directory ${dir}`));
         }
     }
 
-    async writeFile(fileName, text) {
+    async copyDir(source, destDir) {
+        const files = await fsp.readdir(source, {withFileTypes: true});
+        files.forEach((file) => {
+            if (file.isFile()) {
+                this.copyFile(source, destDir, file.name);
+            }
+        });
+    }
+
+    async copyFile(source, destDir, file) {
         try {
-            await fs.writeFile(fileName, text, {flag: 'a'});
+            await fsp.copyFile(path.join(source, `/${file}`), path.join(destDir, `/${file}`));
+            console.log('Copy', source + file, '=>', destDir + file);
         } catch (err) {
-            console.error(`Got an error trying to write to a file ${fileName}: ${err.message}`);
+            console.log(err);
+        }
+    }
+
+    async readFile(file) {
+        const stream = fs.ReadStream(file);
+        const chunks = [];
+        for await (const chunk of stream) {
+            chunks.push(chunk);
+        }
+        return chunks.toString().trim();
+    }
+
+    async createHtmlFile() {
+        const template = await this.readFile(path.join(__dirname, 'template.html'));
+        const header = await this.readFile(path.join(__dirname, '/components/header.html'));
+        const articles = await this.readFile(path.join(__dirname, '/components/articles.html'));
+        const footer = await this.readFile(path.join(__dirname, '/components/footer.html'));
+        const result = template.replace('{{header}}', `${header}`).replace('{{articles}}', `${articles}`).replace('{{footer}}', `${footer}`);
+
+        await fsp.writeFile(path.join(this.projectDir, '/index.html'), `${result}`, {flag: 'w'}, error => {
+            if (error) throw error;
+        });
+    }
+
+    async createCssFile() {
+        const stylesPath = path.join(__dirname, '/styles');
+        const files = await fsp.readdir(stylesPath, {withFileTypes: true});
+        for (let file of files) {
+            console.log(file.name)
+            if (file.isFile() && path.extname(file.name) === '.css') {
+                const css = await this.readFile(path.join(stylesPath, `/${file.name}`));
+                fsp.appendFile(path.join(this.projectDir, '/style.css'), css, {flag: 'a'}, err => {
+                    if (err) throw err;
+                });
+            }
         }
     }
 
